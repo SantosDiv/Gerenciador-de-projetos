088fbe6225a93bf4dfc451bc4407294811feb2bf
Diogenes <diogenesfelipe.s@outlook.com.br>
2026-01-19 01:33:03 -0300

    feat: add titles to route meta for New, Edit, and Search pages

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,19 +15,19 @@ const routes = [
     path: '/new',
     name: 'New',
     component: New,
-    meta: { showHeader: true, showSearchIcon: false }
+    meta: { showHeader: true, showSearchIcon: false, title: 'Criar Projeto' }
   },
   {
     path: '/edit/:id',
     name: 'Edit',
     component: Edit,
-    meta: { showHeader: true, showSearchIcon: false }
+    meta: { showHeader: true, showSearchIcon: false, title: 'Editar Projeto' }
   },
   {
     path: '/search',
     name: 'Search',
     component: Search,
-    meta: { showHeader: false, showSearchIcon: false }
+    meta: { showHeader: false, showSearchIcon: false, title: 'Resultados de busca' }
   }
 ]
 
@@ -35,5 +35,10 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach((to, _from, next) => {
+  document.title = to.name === 'Home' ? 'Gerenciador de projetos' : `Gerenciador de projetos - ${to.meta.title || to.name}`;
+  next();
+});
 
 export default router
